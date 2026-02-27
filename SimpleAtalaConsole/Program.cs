4e3227f9b80710b0d771108b686a35a6c697f135
Atalasoft Support <developersupport@atalasoft.com>
2026-02-27 10:20:53 -0500

    Initial Commit

---
+++ SimpleAtalaConsole/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Atalasoft.Imaging;
+using System.IO;
+using System.Drawing;
+using Atalasoft.Imaging.Codec;
+
+namespace SimpleAtalaConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SimpleAtalaConsole Starting...");
+            string imgPath = GetWorkingDir();
+            string inFile = imgPath + "_CUSTFILES\\Tiger.jpg";
+            Console.WriteLine("  inFile: " + inFile);
+            string outFile = imgPath + "out.png";
+            Console.WriteLine("  outFile: " + outFile);
+
+            Console.WriteLine("BEGIN Processing");
+
+
+            AtalaImage img = new AtalaImage(inFile);
+            Console.WriteLine("  Hi, I have an AtalaImage...");
+            Console.WriteLine("    Size: " + img.Size.ToString());
+            Console.WriteLine("    PixelFormat: " + img.PixelFormat.ToString());
+            img.Dispose();
+
+            Console.WriteLine("END Processing");
+            
+            Console.WriteLine("SimpleAtalaConsole finished... press RETURN to exit");
+            Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// Convenience method to get the root directory of the project - really only useful for debugging
+        /// </summary>
+        /// <returns></returns>
+        private static string GetWorkingDir()
+        {
+            string cwd = System.IO.Directory.GetCurrentDirectory();
+            //Console.WriteLine("cwd is '{0}'", cwd);
+
+            if (cwd.EndsWith("\\bin\\Debug"))
+            {
+                cwd = cwd.Replace("\\bin\\Debug", "\\..\\..\\");
+                //cwd = cwd.Replace("\\bin\\Debug", "\\");
+                //Console.WriteLine("updated cwd is '{0}'", cwd);
+            }
+            return cwd;
+        }
+
+    }
+}
---
+++ SimpleAtalaConsole/README.txt
@@ -0,0 +1,9 @@
+﻿SimpleAtalaConsole
+is a bare bones console app that is a great minimal starting point for repro solutions and testing code
+
+It's got the absolute minimum needed to get a console appliaction that does "something" with Atalasoft
+
+The 
+AtalaIamge img = new AtalaImage();
+is kind of Atalasoft's "Hello World"
+
